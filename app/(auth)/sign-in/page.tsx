ca0876bb1dab64fd4525f8a55411c0e7f344061d
MPAM DEV03 <mpamdev03@MPAMs-Air.true.true>
2024-05-13 22:32:10 +0700

    Implement Server Actions and Mutations

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import AuthForm from '@/components/AuthForm'
 
 // sniped 'rafce'
 const SignIn = () => {
   return (
-    <div>SignIn</div>
+    <section className=' flex-center size-full max-sm:px-6'>
+      <AuthForm type="sign-in" />
+    </section>
   )
 }
 
--- lib/utils.ts
+++ lib/utils.ts
@@ -2,7 +2,7 @@
 import { type ClassValue, clsx } from "clsx";
 import qs from "query-string";
 import { twMerge } from "tailwind-merge";
-// import { z } from "zod";
+import { z } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -209,3 +209,24 @@ export const getTransactionStatus = (date: Date) => {
 //   email: z.string().email(),
 //   password: z.string().min(8),
 // })
+
+export const authFormSchema = (type:string) => z.object({
+  // sign up
+  firstName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  lastName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  address1: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  city: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  state:type === 'sign-in' ? z.string().optional() :  z.string().min(3),
+  postalCode: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  dateOfBirth: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  ssn: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+
+  // both
+  email: z.string().email(),
+  password: z.string().min(8),
+
+  
+  // username: z.string().min(2, {
+  //   message: "Username must be at least 2 characters.",
+  // }),
+})
